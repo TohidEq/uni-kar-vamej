1169ce3e32329509efeb5b3135e69fe96d981343
Tohid Eghdami <TohidEq@gmail.com>
2025-06-05 19:20:15 +0330

    fix: :bug: Search Persian keywords  in Local Keywords Bank

--- src/utils/wordsUtils.ts
+++ src/utils/wordsUtils.ts
@@ -1,8 +1,11 @@
 "use client";
 
 export const getFileNameOfWord = (
-  word: string
+  keyword: string
 ): { isExist: boolean; fileName: string } => {
+  const word = decodeURIComponent(keyword).replaceAll("+", " ");
+
+  console.log(`Check \t"${word}"\t Word in LocalStorage.keywords...`);
   const storedData = localStorage.getItem("keywords");
   if (storedData) {
     try {
@@ -14,17 +17,22 @@ export const getFileNameOfWord = (
             if (
               keywords[keyword].relatives.includes(word.toLowerCase().trim())
             ) {
+              console.log(
+                `========\nThis \t"${word}"\t Word has been founded in LocalStorage.keywords\n \t Filename: ${keywords[keyword].file_name}\n========`
+              );
               return { isExist: true, fileName: keywords[keyword].file_name };
             }
           }
         }
       }
     } catch (error) {
+      console.log(`========\n!! This \t"${word}"\t Word not found\n========`);
       console.error("Error parsing keywords from localStorage:", error);
       // Return false in case of parsing error
       return { isExist: false, fileName: "" };
     }
   }
 
+  console.log(`========\n!! This \t"${word}"\t Word not found\n========`);
   return { isExist: false, fileName: "" };
 };
